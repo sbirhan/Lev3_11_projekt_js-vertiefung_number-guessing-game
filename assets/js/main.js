73450aed4354f294fb00921cdd322e1e05180e93
sbirhan <sbirhan@gmail.com>
2020-06-06 15:21:22 +0200

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,19 @@ var movingPigeon = setInterval(walkingPigeon, 2500);
 
 var secretNumber = Math.floor(Math.random() * 101);
 const estimation = document.getElementById("cloud");
-document.getElementById("sun").addEventListener("click", flyingPigeon);
 
 const sun = document.getElementById("sun");
+sun.addEventListener("click", flyingPigeon);
 
 const restart = document.getElementById("restart");
-document.getElementById("restart").addEventListener("click", reset);
-document.getElementById("restart").style.visibility = "hidden";
+restart.addEventListener("click", reset);
+restart.style.visibility = "hidden";
 
-function walkingPigeon(){
+const gameNumberLimit = document.getElementById("rangeSlider");
+var gameNumber = gameNumberLimit.value;
+
+
+function walkingPigeon(){    
     document.getElementById("restart").style.visibility = "hidden";
     if (Math.floor(Math.random() * 2) == 0){
         angle = Math.floor(Math.random() * 91);
@@ -33,36 +37,41 @@ function walkingPigeon(){
     Pigeon.style.left = left2 + "vw";
     top1 = top2;
     left1 = left2;
-
-    console.log(angle, Math.round(16 * Math.sin(angle/180)), Math.round(16 * Math.cos(angle/180)), left1, top1, left2, top2);
-
 }
 
 function flyingPigeon(){
-    if (estimation.value == secretNumber){
-        clearInterval(movingPigeon);
-        estimation.style.visibility = "hidden";
-        document.getElementById("blackCloud").style.visibility = "hidden";
-        left2 = 60;
-        top2 = -15;
-        document.getElementById("pigeonOnRoofImage").src = "../assets/img/pigeonFlying.png"
-        Pigeon.animate([{left: left1 + "vw"}, {left: left2 + "vw"}],{duration: 6000,direction: "normal"});
-        Pigeon.animate([{top: top1 + "vw"}, {top: top2 + "vw"}],{duration: 6000, direction: "normal"});
-        Pigeon.animate([{opacity: "1"}, {opacity: "0"}],{duration: 6000, direction: "normal"});
-        Pigeon.animate([{width: "10%"}, {width: "5%"}],{duration: 6000, direction: "normal"});
-        Pigeon.style.top = top2 + "vw";
-        Pigeon.style.left = left2 + "vw";
-        Pigeon.style.opacity = "0";
-        sun.disabled = "true";
-        document.getElementById("restart").style.visibility = "visible";
-    }
-    else{
-        if (estimation.value > secretNumber){
-            sun.animate([{backgroundColor: "rgb(255, 255, 255)"}, {backgroundColor: "rgb(255, 153, 0)"}],{duration: 2000});
+    if (gameNumber != 0) {
+        gameNumberLimit.value = gameNumber;
+        if (estimation.value == secretNumber){
+            clearInterval(movingPigeon);
+            estimation.style.visibility = "hidden";
+            document.getElementById("blackCloud").style.visibility = "hidden";
+            left2 = 60;
+            top2 = -15;
+            document.getElementById("pigeonOnRoofImage").src = "../assets/img/pigeonFlying.png"
+            Pigeon.animate([{left: left1 + "vw"}, {left: left2 + "vw"}],{duration: 6000,direction: "normal"});
+            Pigeon.animate([{top: top1 + "vw"}, {top: top2 + "vw"}],{duration: 6000, direction: "normal"});
+            Pigeon.animate([{opacity: "1"}, {opacity: "0"}],{duration: 6000, direction: "normal"});
+            Pigeon.animate([{width: "10%"}, {width: "5%"}],{duration: 6000, direction: "normal"});
+            Pigeon.style.top = top2 + "vw";
+            Pigeon.style.left = left2 + "vw";
+            Pigeon.style.opacity = "0";
+            sun.disabled = "true";
+            document.getElementById("restart").style.visibility = "visible";
         }
         else{
-            sun.animate([{color: "rgb(255, 153, 0)"}, {color: "rgb(255, 255, 255"}],{duration: 2000});
+            if (estimation.value > secretNumber){
+                sun.animate([{backgroundColor: "rgb(255, 255, 255)"}, {backgroundColor: "rgb(255, 153, 0)"}],{duration: 2000});
+            }
+            else{
+                sun.animate([{color: "rgb(255, 153, 0)"}, {color: "rgb(255, 255, 255"}],{duration: 2000});
+            }
         }
+        gameNumber--;
+    }
+    else {
+        sun.removeEventListener("click", flyingPigeon);
+        sun.disabled = "true";
     }
 }
 
